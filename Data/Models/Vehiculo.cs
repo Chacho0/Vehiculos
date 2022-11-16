64d197d1b734c08caea3c6736f501a37be615d11
Chacho0 <hectorrafaeljaquezlanfrancon@gmail.com>
2022-11-16 14:20:44 -0400

    0.1

---
+++ Data/Models/Vehiculo.cs
@@ -0,0 +1,9 @@
+namespace Vehiculo_20200671.Data.Models;
+public class vehiculo
+{
+  public string? VehiculoID{ get; set; } 
+  public string? Marca{ get; set; } 
+  public string? Modelo{ get; set; } 
+  public string? Año{ get; set; } 
+  public string? Color{ get; set; }  
+}
